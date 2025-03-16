6769ac68f14c03c00417c79b602f09bfcbbe3e2a
ensp1re <ostapuk1318@gmail.com>
2025-03-16 02:54:12 +0200

    fixed some bags

--- lib/tronweb.ts
+++ lib/tronweb.ts
@@ -4,8 +4,7 @@ const { TronWeb } = require("tronweb");
 
 const fullNode = "https://api.trongrid.io";
 const apiKey =
-  process.env.NEXT_PUBLIC_TRONGRID_API_KEY ||
-  "1e0828cd-9eab-4e80-9dd6-b76672882e8d";
+  process.env.NEXT_PUBLIC_TRONGRID_API_KEY!;
 
 // Initialize TronWeb
 const tronWeb = new TronWeb({
